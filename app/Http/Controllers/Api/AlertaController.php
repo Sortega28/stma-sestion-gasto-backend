f18d1e842a8e3f1a9d323c0566444d9a74cf5b8e
Sara Ortega <sortegapoz@uoc.edu>
2025-12-01 12:12:11 +0100

    modulo alertas

--- app/Http/Controllers/Api/AlertaController.php
+++ app/Http/Controllers/Api/AlertaController.php
@@ -26,12 +26,13 @@ class AlertaController extends Controller
 
         /*gasto anual REAL por proveedor basado en la tabla "solicitudgasto" y su campo fechaalta*/
         $subGastoAnual = DB::table('solicitudgasto as sg')
-            ->select(
-                'sg.nombre_prov_final as proveedor',
-                DB::raw('SUM(sg.importe) AS gasto_anual')
-            )
-            ->whereYear('sg.fechaalta', $anioActual)
-            ->groupBy('sg.nombre_prov_final');
+    ->select(
+        'sg.nombre_prov_final as proveedor',
+        'sg.objeto',
+        DB::raw('SUM(sg.importe) AS gasto_anual')
+    )
+    ->whereYear('sg.fechaalta', $anioActual)
+    ->groupBy('sg.nombre_prov_final', 'sg.objeto');
 
          // --- ALERTAS DEL AÑO ACTUAL basadas en la columna "anio" ---
         $q = DB::table('alertas')->where('alertas.anio', $anioActual);
@@ -54,8 +55,10 @@ class AlertaController extends Controller
 
         // JOIN con gasto anual real desde solicitudgasto
         $q->leftJoinSub($subGastoAnual, 'totales', function ($join) {
-            $join->on('totales.proveedor', '=', 'alertas.proveedor');
-        });
+    $join->on('totales.proveedor', '=', 'alertas.proveedor')
+         ->on('totales.objeto', '=', 'alertas.objeto');
+});
+
 
         // Selección final
         $q->select('alertas.*', 'totales.gasto_anual');
